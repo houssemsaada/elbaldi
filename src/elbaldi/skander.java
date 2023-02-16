0660621d8045fe03e89052c3343cc3ef51ecdad5
skander <skander.mammouri@esprit.tn>
2023-02-16 19:46:48 +0100

    CRUD and Filters

--- src/elbaldi/skander.java
+++ src/elbaldi/skander.java
@@ -5,10 +5,18 @@
  */
 package elbaldi;
 
+import elbaldi.models.Evenement;
+import elbaldi.services.EvenementServices;
+
 /**
  *
  * @author MSI
  */
 public class skander {
-    
+      EvenementServices es = new EvenementServices();
+        
+        Evenement e1= new Evenement(10, "amen", "ssdddes", "15-10-2021", "19-10-2023", "100dt");
+       es.ajouter(e1);
+      es.supprimer(e1);
+        System.out.println(es.recuperer(e1));   
 }
